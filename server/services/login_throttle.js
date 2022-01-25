24e82dc2f86f9c42a1be78cf90e197969f466671
HarryAustin <osagiedeharrison@yahoo.com>
2022-01-25 21:54:28 +0100

    update throttle logic

--- server/services/login_throttle.js
+++ server/services/login_throttle.js
@@ -1,11 +1,11 @@
 const limitter = require("express-rate-limit");
 
 const loginLimitter = limitter({
-    windowMs: 60*2000,
-    max: 3,
-    handler = (req, res, next) => {
-        res.status(429).send('Sorry, please try again in 2 mins')
-    }
-})
+  windowMs: 60 * 2000,
+  max: 3,
+  handler: (req, res, next) => {
+    res.status(429).send("Sorry, please try again in 2 mins");
+  },
+});
 
-exports.loginLimitter = loginLimitter
+exports.loginLimitter = loginLimitter;
