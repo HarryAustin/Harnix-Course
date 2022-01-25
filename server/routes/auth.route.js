1f623743d21ea84db1cca87594d26f31084d8912
HarryAustin <osagiedeharrison@yahoo.com>
2022-01-25 21:52:08 +0100

    throttle login request

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const Route = express.Router();
 const { registerUser, loginUser } = require("../controllers/auth.controller");
+const { loginLimitter } = require('../services/login_throttle')
 
 Route.post("/register", registerUser);
-Route.post("/login", loginUser);
+Route.post("/login", loginLimitter, loginUser);
 module.exports = Route;
